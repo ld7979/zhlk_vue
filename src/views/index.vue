cf885f064ac2f79de686a8f86446bcbbf16c0efb
LiDian <Ld@tedu.cn>
2019-07-03 11:33:12 +0800

    模板

---
+++ src/views/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+         <div style="width:500px;height:500px" ref="chart"></div>
+    </div>
+</template>
+<script>
+export default{
+   data () {
+　　return {};
+　},
+　methods: {
+　　initCharts () {
+　　let myChart = this.$echarts.init(this.$refs.chart);
+　　console.log(this.$refs.chart)
+　　// 绘制图表
+　　myChart.setOption({
+　　title: { text: '在Vue中使用echarts' },
+　　tooltip: {},
+　　xAxis: {
+　　data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+　　},
+　　yAxis: {},
+　　series: [{
+　　name: '销量',
+　　type: 'bar',
+　　data: [5, 20, 36, 10, 10, 20]
+　　}]
+　　});
+　　}
+　  },
+　  mounted () {
+　　this.initCharts();
+　}
+}
+</script>
