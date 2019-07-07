5b324ef703f28ba3b5941cebb45f50505d11b1ad
lidian <ld_9977@163.com>
2019-07-07 19:28:57 +0800

    头部提交

---
+++ src/components/background.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <div class="header_bg">
+      <div class="head_left">
+        <span>智慧粮库运营监管平台</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {}
+  }
+}
+</script>
+
+<style>
+@import '../assets/css/background.css' 
+</style>
+
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,35 +1,41 @@
 <template>
     <div>
-         <div style="width:500px;height:500px" ref="chart"></div>
+      <div style="width:500px;height:500px" ref="chart"></div>
     </div>
 </template>
 <script>
 export default{
-   data () {
+  data () {
 　　return {};
 　},
 　methods: {
 　　initCharts () {
-　　let myChart = this.$echarts.init(this.$refs.chart);
-　　console.log(this.$refs.chart)
-　　// 绘制图表
-　　myChart.setOption({
-　　title: { text: '在Vue中使用echarts' },
-　　tooltip: {},
-　　xAxis: {
-　　data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-　　},
-　　yAxis: {},
-　　series: [{
-　　name: '销量',
-　　type: 'bar',
-　　data: [5, 20, 36, 10, 10, 20]
-　　}]
-　　});
+　　  let myChart = this.$echarts.init(this.$refs.chart);
+　  　console.log(this.$refs.chart)
+　  　// 绘制图表
+　  　myChart.setOption({
+　　    title : {
+            text: '某站点用户访问来源',
+            x:'center'
+        },
+        series : [
+            {
+                type: 'pie',
+                radius: ['50%', '70%'],
+                center: ['50%', '60%'],
+                data:[
+                    {value:335, name:'省级储备',itemStyle:{color:'#ff0'}},
+                    {value:310, name:'城市储备',itemStyle:{color:'#f0f'}},
+                    {value:234, name:'商品粮',itemStyle:{color:'#f00'}},
+                    {value:135, name:'中央储备',itemStyle:{color:'#0ff'}}
+                ]
+            }
+        ] 
+　　  });
 　　}
-　  },
-　  mounted () {
+　},
+　mounted () {
 　　this.initCharts();
 　}
 }
-</script>
+</script> 
